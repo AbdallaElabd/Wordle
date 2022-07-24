44420be58f3158e47ea0fe67ccc70f586820c37a
Abdalla Elabd <abdalla.mohamed.elabd@gmail.com>
2022-07-24 14:26:41 +0200

    add prettier config to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,16 +18,19 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'prettier'],
   settings: {
     react: {
       version: 'detect'
     }
   },
   rules: {
+    // Linting
+    indent: 'off',
     'space-before-function-paren': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
+    'prettier/prettier': 'error',
 
     // Jest
     'jest/no-disabled-tests': 'warn',
